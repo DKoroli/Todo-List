c5c5e9f29017851c3d93799f8b2137a5372e5545
DKoroli <dimakoroliuk@gmail.com>
2023-03-14 06:11:01 +0200

    14.03.23

--- todo.js
+++ todo.js
@@ -42,9 +42,10 @@ const tasks = [
   const inputTitle = form.elements["title"];
   const inputBody = form.elements["body"];
 
-//   Events
+  //   Events
   renderAllTasks(objOfTasks);
-  form.addEventListener('submit', onFormSubmitHandler);
+  form.addEventListener("submit", onFormSubmitHandler);
+  listContainer.addEventListener("click", onDeleteHandler);
 
   function renderAllTasks(tasksList) {
     if (!tasksList) {
@@ -58,35 +59,82 @@ const tasks = [
     });
     listContainer.appendChild(fragment);
   }
-    function listItemsTemplate({ _id, title, body } = {}) {
-      const li = document.createElement("li");
-      li.classList.add(
-        "list-group-item",
-        "d-flex",
-        "align-items-center",
-        "flex-wrap",
-        "mt-2"
-      );
-      const span = document.createElement("span");
-      span.textContent = title;
-      span.style.fontWeight = "bold";
+  function listItemsTemplate({ _id, title, body } = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "flex-wrap",
+      "mt-2"
+    );
+    li.setAttribute("data-task-id", _id);
 
-      const deleteBtn = document.createElement("button");
-      deleteBtn.textContent = "Delete task";
-      deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+    const span = document.createElement("span");
+    span.textContent = title;
+    span.style.fontWeight = "bold";
 
-      const article = document.createElement("p");
-      article.textContent = body;
-      article.classList.add("mt-2", "w-100");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete task";
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
 
-      li.appendChild(span);
-      li.appendChild(deleteBtn);
-      li.appendChild(article);
+    const article = document.createElement("p");
+    article.textContent = body;
+    article.classList.add("mt-2", "w-100");
 
-      return li;
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert("Пожалуйста введите Title и Body");
+      return;
     }
 
-    function onFormSubmitHandler(e) {
-        e.preventDefault();
+    const task = creatNewTask(titleValue, bodyValue);
+    const listItem = listItemsTemplate(task);
+    listContainer.insertAdjacentElement("afterbegin", listItem);
+    form.reset();
+  }
+
+  function creatNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `Task-${Math.random()}`,
+    };
+    objOfTasks[newTask._id] = newTask;
+    return { ...newTask };
+  }
+
+  function deleteTask(id) {
+    const { title } = objOfTasks[id];
+    const isConfirm = confirm(`Вы точно хотите удалить задачу; ${title}?`);
+    if (!isConfirm) return isConfirm;
+    delete objOfTasks[id];
+    return isConfirm;
+  }
+
+  function deleteTaskFromHtml(confirmed, el) {
+    if (!confirmed) return;
+    el.remove();
+  }
+
+  function onDeleteHandler({ target }) {
+    if (target.classList.contains("delete-btn")) {
+      const parent = target.closest("[data-task-id]");
+      const id = parent.dataset.taskId;
+      const confirmed = deleteTask(id);
+      deleteTaskFromHtml(confirmed, parent);
+    }
   }
 })(tasks);
